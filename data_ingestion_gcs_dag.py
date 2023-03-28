1a32cab8698ac46afdf998cc0298f3ff02a410fd
Reviandi Naufal <reviandinaufalk@gmail.com>
2023-03-28 11:58:53 +0700

    Edit DAGs and add variable.json file for airflow composer

--- data_ingestion_gcs_dag.py
+++ data_ingestion_gcs_dag.py
@@ -15,4 +15,73 @@ PROJECT_ID = Variable.get("PROJECT_ID")
 BUCKET = Variable.get("BUCKET_NAME")
 
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'session2_bq')
+# BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'session2_bq')
+
+# NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
+def upload_to_gcs(bucket, object_dir, local_glob):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    :param bucket: GCS bucket name
+    :param object_name: target path & file-name
+    :param local_file: source path & file-name
+    :return:
+    """
+
+    # logging.info(f"bucket nama: {bucket}")
+    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+    # End of Workaround
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+
+    for ifile in glob.glob(local_glob):
+        filename = os.path.basename(ifile)
+        blob = bucket.blob(f"{object_dir}/{filename}")
+        blob.upload_from_filename(ifile)
+
+
+default_args = {
+    "owner": "airflow",
+    "start_date": days_ago(1),
+    "depends_on_past": False,
+    "retries": 1,
+}
+
+# NOTE: DAG declaration - using a Context Manager (an implicit way)
+with DAG(
+    dag_id="data_ingestion_gcs_dag",
+    schedule_interval="@daily",
+    default_args=default_args,
+    catchup=False,
+    max_active_runs=1,
+    tags=['dtc-de'],
+) as dag:
+
+    # download_dataset_task = BashOperator(
+    #     task_id="download_dataset_task",
+    #     #bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
+    #     bash_command=f"for m in {{01..3}}; do wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-$m.parquet -P {path_to_local_home}; done"
+    # )
+
+    # format_to_parquet_task = PythonOperator(
+    #     task_id="format_to_parquet_task",
+    #     python_callable=format_to_parquet,
+    #     op_kwargs={
+    #         "src_file": f"{path_to_local_home}/{dataset_file}",
+    #     },
+    # )
+
+    local_to_gcs_task = PythonOperator(
+        task_id="local_to_gcs_task",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_dir": f"raw_data",
+            "local_glob": f"{path_to_local_home}/*.parquet",
+        },
+    )
+
+    download_dataset_task >> local_to_gcs_task
