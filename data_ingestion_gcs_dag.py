e309e1568c97599f5d7291e292a864376aaf919b
Reviandi Naufal <reviandinaufalk@gmail.com>
2023-04-04 13:08:39 +0700

    add ingesting process with check latest data

--- data_ingestion_gcs_dag.py
+++ data_ingestion_gcs_dag.py
@@ -1,51 +1,23 @@
 import os
-# import glob
-# import logging
 # from copy_bucket_to_bucket import copy_objects
 # from load_data_gcs_to_bigquery import load_gcs_to_bq
 
 from airflow import DAG
 from airflow.utils.dates import days_ago
-# from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-# from airflow.models import Variable
 
 from google.cloud import storage
 from google.cloud import bigquery
-# from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
 
-# PROJECT_ID = Variable.get("PROJECT_ID")
-# BUCKET = Variable.get("BUCKET_NAME")
+def extract_number_source(f):
+    s = re.findall(r"test_data/online_payment_(\d+).parquet",f)
+    print(s)
+    return (int(s[0]) if s else -1,f)
 
-# path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-# BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'session2_bq')
-
-# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'D:\DATA\Data Fellowship IYKRA\CODE\Final Project\fraud-project1-d4d121a8c66e.json'
-
-# NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
-# def upload_to_gcs(bucket, object_dir, local_glob):
-#     """
-#     Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
-#     :param bucket: GCS bucket name
-#     :param object_name: target path & file-name
-#     :param local_file: source path & file-name
-#     :return:
-#     """
-
-#     # logging.info(f"bucket nama: {bucket}")
-#     # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
-#     # (Ref: https://github.com/googleapis/python-storage/issues/74)
-#     storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
-#     storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
-#     # End of Workaround
-
-#     client = storage.Client()
-#     bucket = client.bucket(bucket)
-
-#     for ifile in glob.glob(local_glob):
-#         filename = os.path.basename(ifile)
-#         blob = bucket.blob(f"{object_dir}/{filename}")
-#         blob.upload_from_filename(ifile)
+def extract_number_dest(f):
+    s = re.findall(r"dataset_test/test_data/online_payment_(\d+).parquet",f)
+    print(s)
+    return (int(s[0]) if s else -1,f)
 
 def copy_objects(source_bucket_name, destination_bucket_name, prefix=''):
     """Copies a blob from one bucket to another."""
@@ -58,20 +30,43 @@ def copy_objects(source_bucket_name, destination_bucket_name, prefix=''):
     source_bucket = storage_client.bucket(source_bucket_name)
     destination_bucket = storage_client.bucket(destination_bucket_name)
 
-    for blob in source_bucket.list_blobs(prefix=prefix):
-        source_blob = source_bucket.blob(blob.name)
-        destination_blob_name = f"dataset/{blob.name}"
+    source_blobs_list = []
+    dest_blobs_list = []
 
-        blob_copy = source_bucket.copy_blob(source_blob, destination_bucket, destination_blob_name)
+    for blob_source in source_bucket.list_blobs(prefix=prefix):
+        source_blobs_list.append(str(blob_source.name))
+    
+    max_file_source = max(source_blobs_list,key=extract_number_source)
+    num_source = re.findall(r'cleaned_dataset/online_payment_(\d+).parquet', max_file_source)
+    max_num_source = (int(num_source[0]) if num_source else -1)
 
-        print(
-            "Blob {} in bucket {} copied to blob {} in bucket {}.".format(
-                source_blob.name,
-                source_bucket.name,
-                blob_copy.name,
-                destination_bucket.name,
+    for blob_dest in destination_bucket.list_blobs(prefix="dataset_test/"):
+        dest_blobs_list.append(str(blob_dest.name))
+
+    max_file_dest = max(dest_blobs_list,key=extract_number_dest)
+    num_dest = re.findall(r'dataset/cleaned_dataset/online_payment_(\d+).parquet', max_file_dest)
+    max_num_dest = (int(num_dest[0]) if num_dest else -1)
+
+    print(f"Max file number in source: {max_num_source}")
+    print(f"Max file number in source: {max_num_dest}")
+
+    if max_num_source > max_num_dest:
+        for blob in source_bucket.list_blobs(prefix=prefix):
+            source_blob = source_bucket.blob(blob.name)
+            destination_blob_name = f"dataset_test/{blob.name}"
+
+            blob_copy = source_bucket.copy_blob(source_blob, destination_bucket, destination_blob_name)
+
+            print(
+                "Blob {} in bucket {} copied to blob {} in bucket {}.".format(
+                    source_blob.name,
+                    source_bucket.name,
+                    blob_copy.name,
+                    destination_bucket.name,
+                )
             )
-        )
+    else:
+        print("Your datalake is up to date")
 
 def load_gcs_to_bq(dataset_name, table_name, dataset_loc, bq_schema, uri):
     # Construct a BigQuery client object.
@@ -158,29 +153,4 @@ with DAG(
         },
     )
 
-    # download_dataset_task = BashOperator(
-    #     task_id="download_dataset_task",
-    #     #bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
-    #     bash_command=f"for m in {{1..743}}; do wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -P {path_to_local_home}; done"
-    # )
-
-    # format_to_parquet_task = PythonOperator(
-    #     task_id="format_to_parquet_task",
-    #     python_callable=format_to_parquet,
-    #     op_kwargs={
-    #         "src_file": f"{path_to_local_home}/{dataset_file}",
-    #     },
-    # )
-
-    # local_to_gcs_task = PythonOperator(
-    #     task_id="local_to_gcs_task",
-    #     python_callable=upload_to_gcs,
-    #     op_kwargs={
-    #         "bucket": BUCKET,
-    #         "object_dir": f"raw_data",
-    #         "local_glob": f"{path_to_local_home}/*.parquet",
-    #     },
-    # )
-
-    # download_dataset_task >> local_to_gcs_task
     ingest_to_dataLake >> load_to_bq
