9a7a89dba0c27f52e3993d1e8f7568d3f6bc1791
Reviandi Naufal <reviandinaufalk@gmail.com>
2023-04-04 13:09:42 +0700

    update testing code for ingesting data with latest data check func

--- copy_bucket_to_bucket.py
+++ copy_bucket_to_bucket.py
@@ -1,7 +1,18 @@
 import os
+import re
 from google.cloud import storage
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'D:\DATA\Data Fellowship IYKRA\CODE\Final Project\fraud-project1-d4d121a8c66e.json'
+
+def extract_number_source(f):
+    s = re.findall(r"test_data/online_payment_(\d+).parquet",f)
+    print(s)
+    return (int(s[0]) if s else -1,f)
+
+def extract_number_dest(f):
+    s = re.findall(r"dataset_test/test_data/online_payment_(\d+).parquet",f)
+    print(s)
+    return (int(s[0]) if s else -1,f)
 
 def copy_objects(source_bucket_name, destination_bucket_name, prefix=''):
     """Copies a blob from one bucket to another."""
@@ -14,23 +25,46 @@ def copy_objects(source_bucket_name, destination_bucket_name, prefix=''):
     source_bucket = storage_client.bucket(source_bucket_name)
     destination_bucket = storage_client.bucket(destination_bucket_name)
 
-    for blob in source_bucket.list_blobs(prefix=prefix):
-        source_blob = source_bucket.blob(blob.name)
-        destination_blob_name = f"dataset/{blob.name}"
+    source_blobs_list = []
+    dest_blobs_list = []
 
-        blob_copy = source_bucket.copy_blob(source_blob, destination_bucket, destination_blob_name)
+    for blob_source in source_bucket.list_blobs(prefix=prefix):
+        source_blobs_list.append(str(blob_source.name))
+    
+    max_file_source = max(source_blobs_list,key=extract_number_source)
+    num_source = re.findall(r'test_data/online_payment_(\d+).parquet', max_file_source)
+    max_num_source = (int(num_source[0]) if num_source else -1)
 
-        print(
-            "Blob {} in bucket {} copied to blob {} in bucket {}.".format(
-                source_blob.name,
-                source_bucket.name,
-                blob_copy.name,
-                destination_bucket.name,
+    for blob_dest in destination_bucket.list_blobs(prefix="dataset_test/"):
+        dest_blobs_list.append(str(blob_dest.name))
+
+    max_file_dest = max(dest_blobs_list,key=extract_number_dest)
+    num_dest = re.findall(r'dataset_test/test_data/online_payment_(\d+).parquet', max_file_dest)
+    max_num_dest = (int(num_dest[0]) if num_dest else -1)
+
+    print(f"Max file number in source: {max_num_source}")
+    print(f"Max file number in source: {max_num_dest}")
+
+    if max_num_source > max_num_dest:
+        for blob in source_bucket.list_blobs(prefix=prefix):
+            source_blob = source_bucket.blob(blob.name)
+            destination_blob_name = f"dataset_test/{blob.name}"
+
+            blob_copy = source_bucket.copy_blob(source_blob, destination_bucket, destination_blob_name)
+
+            print(
+                "Blob {} in bucket {} copied to blob {} in bucket {}.".format(
+                    source_blob.name,
+                    source_bucket.name,
+                    blob_copy.name,
+                    destination_bucket.name,
+                )
             )
-        )
+    else:
+        print("Your datalake is up to date")
 
 if __name__ == '__main__':
     source_bucket_name ='dataset-fraud'
     destination_bucket_name = 'asia-southeast1-final-proje-f3d92743-bucket'
-    prefix = "cleaned_dataset/" #optional, use when the data in the bucket is in another folder  
+    prefix = "test_data/" #optional, use when the data in the bucket is in another folder  
     copy_objects(source_bucket_name, destination_bucket_name, prefix)
